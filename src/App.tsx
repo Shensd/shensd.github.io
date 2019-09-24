c5f2ab9f9ef56a5c0113bcc47fa6c45ec0411c2b
Jack Hance <jack_e_hance@yahoo.com>
2019-09-24 15:04:37 -0500

    Reformatted the page, added images, added logolink, changed background styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,31 @@ import InfoCard from './components/infocard';
 import Jumbotron from './components/jumbotron';
 import LogoLink from './components/logolink';
 
+import NameplateCPP from './res/nameplates/cpp-nameplate.png';
+
+import Logo from "./res/spiral-mini.png";
+
 const App: React.FC = () => {
     return (
         <div className="App">
             <Jumbotron>
                 <h1>Jack Hance</h1>
                 <PositionCarousel preface="" titles={["Software Developer", "Hacker", "Student", "Circuit Bender", "Researcher"]}/>
+                <LogoLink logo={Logo} link="http://google.com"></LogoLink>
             </Jumbotron>
 
             <div className="cards">
-                <InfoCard title="Education" direction="left">
-                    <p><b>BS Computer Science</b> - North Dakota State University, Expected May 2021</p>
-                    <p>
-                        I am currently attending my sophomore year at North Dakota State University, the computer science 
-                        course is heavily focused on Java, but also includes use of languages such as C# and C.
-                    </p>
-                    <ul>
-                        <li>Recognized as a student of distinction within the NDSU Department of Computer Science</li>
-                        <li>Placed on the dean’s list for fall and spring semesters of freshman year</li>
-                    </ul>
-                </InfoCard>
-
-                <InfoCard title="Extracurricular" direction="left">
+                <InfoCard title="Education">
+                <p><b>BS Computer Science</b> - North Dakota State University, Expected May 2021</p>
+                <p>
+                    I am currently attending my sophomore year at North Dakota State University, the computer science 
+                    course is heavily focused on Java, but also includes use of languages such as C# and C.
+                </p>
+                <ul>
+                    <li>Recognized as a student of distinction within the NDSU Department of Computer Science</li>
+                    <li>Placed on the dean’s list for fall and spring semesters of freshman year</li>
+                </ul>
+                <h2>Extracurricular</h2>
                     <p><b>Club Officer</b> - NDSU Cybersecurity Student Association</p>
                     <p>
                         The NDSU Cybersecurity Student Association is a club dedicated to peer learning oriented towards cybersecurity. 
@@ -40,9 +43,7 @@ const App: React.FC = () => {
                         <li>Self and peer taught in many areas of cybersecurity </li>
                         <li>Worked in a team environment on several software development projects</li>
                     </ul>
-                </InfoCard>
-
-                <InfoCard title="Research" direction="left">
+                <h2>Research</h2>
                     <p><b>NDSU Cybersecurity Research Experience for Undergraduates</b></p>
                     <p>
                         The Research Experience for Undergraduates is a program sponsored by the National Science Foundation to encourage
@@ -51,14 +52,9 @@ const App: React.FC = () => {
                         forensic analysis. My project was specifically about the use of unsupervised learning on profiling users by Bash 
                         history. This project is currently in the process of being published by IEEE.
                     </p>
-                </InfoCard>
-
-                <InfoCard title="Experience" direction="left">
-
-                </InfoCard>
-
-                <InfoCard title="What I've Made" direction="left">
-
+                <h2>Experience</h2>
+                    <img src={NameplateCPP} alt="C++ Nameplate" className="lang-nameplate"/>
+                <h2>What I've Made</h2>
                 </InfoCard>
             </div>
         </div>
--- src/components/logolink.tsx
+++ src/components/logolink.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "./logolink.css";
+
 const LogoLink = ({logo, link} : {logo: string, link: string}) => {
     return (
         <div className="logo-link">
             <a href={link}>
-                <img src={logo} />
+                <img src={logo} className="logo-link-img" />
             </a>
         </div>
     );
