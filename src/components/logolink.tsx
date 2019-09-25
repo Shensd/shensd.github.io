756535d3e9491c362073c8b046dada1852a88b2f
Jack Hance <jack_e_hance@yahoo.com>
2019-09-24 20:53:01 -0500

    Cleaned up files, fixed spelling errors, cleaned up css, added finishing touches to website

--- src/components/logolink.tsx
+++ src/components/logolink.tsx
@@ -6,7 +6,7 @@ const LogoLink = ({logo, link} : {logo: string, link: string}) => {
     return (
         <div className="logo-link">
             <a href={link}>
-                <img src={logo} className="logo-link-img" />
+                <img src={logo} className="logo-link-img" alt={link} />
             </a>
         </div>
     );
