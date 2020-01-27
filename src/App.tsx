7488ba11b307d20fd9f931a1e2ce646c1f20a98a
Jack Hance <jack_e_hance@yahoo.com>
2020-01-26 21:11:08 -0600

    Updated information, rearranged skills to reflect current abilities and interests, rewrote some weaker paragraphs, added achievements sections

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App: React.FC = () => {
                         <li>Placed on the dean’s list for fall and spring semesters of freshman year</li>
                     </ul>
                     <h2>Extracurricular</h2>
-                        <p><b>Club Officer</b> - NDSU Cybersecurity Student Association</p>
+                        <p><b>Vice President</b> - NDSU Cybersecurity Student Association</p>
                         <p>
                             The NDSU Cybersecurity Student Association is a club dedicated to peer learning oriented towards cybersecurity. 
                             Club members participate in student lead lessons and compete in hacking competitions. The club also focuses on 
@@ -54,18 +54,34 @@ const App: React.FC = () => {
                             research to be conducted by undergraduate students in a growing number of different fields. I participated in the
                             NDSU hosted Cybersecurity REU and developed, conducted, and wrote about the use of machine learning in post attack 
                             forensic analysis. My project was specifically about the use of unsupervised learning on profiling users by Bash 
-                            history. This project is currently in the process of being published by IEEE.
+                            history.
+                        </p>
+                    <h2>Achievements</h2>
+                        <p><b>Cyber Skyline’s NCL Regular Season 2019 CTF Competition</b>, placed in the top 1% of 4000+ players</p>
+                        <p>
+                            A Capture the Flag competition is a competition in where players are given a wide array of hacking challenges with the 
+                            goal of having players use many different cybersecurity skills to solve problems to get an end "flag." An example of a 
+                            basic challenge would be to use an SQLI vulnerability to leak a database that holds a specific string that is the final 
+                            flag. Cyber Skyline's NCL CTF competition consisted of categories including cryptography, reverse engineering, log analysis, 
+                            password cracking, web application exploitation, and several others. In the individual section of the competition, 
+                            I placed 37th out of 4,149 players nationwide.
+                        </p>
+                        <p><b>2019 National Cyber Summit Cyber Cup Challenge</b>, placed 2nd out of 10+ college teams</p>
+                        <p>
+                            The National Cyber Summit Cyber Cup Challenge is a team-based CTF taking place live during the Annual National Cyber Summit conference. 
+                            I was the team captain of a team consisting of 4 members. We worked as a team to solve cybersecurity problems in several different 
+                            categories, with a strong focus on web application exploitation.
                         </p>
                     <h2>Experience</h2>
                     <div className="langs">
                         <div className="lang-box">
                             <h3>C++</h3>
-                            <p>2 years experience (2017)</p>
+                            <p>3 years experience (2017)</p>
                             <p>
                                 My primary development language is C++, I tend to use it largely with networking and hardware
                                 but have also used it with libraries such as OpenGL. Some specific projects created with C++
                                 would include things like PSBB, an HTTP web server written from scratch using raw sockets, and 
-                                an Arduino library for working with the HID5455 family of RFID scanners.
+                                an <a href="https://github.com/Shensd/HID5455-Arduino" target="_blank" rel="noopener noreferrer">Arduino library for working with the HID5455 family of RFID scanners.</a>
                             </p>
                             <ul>
                                 <li>Hardware</li>
@@ -74,25 +90,8 @@ const App: React.FC = () => {
                             </ul>
                         </div>
                         <div className="lang-box">
-                            <h3>HTML5/CSS3/Javascript</h3>
-                            <p>4 years experience (2015)</p>
-                            <p>
-                                I have used front end web development technologies frequently throughout my career as a 
-                                developer, and I find myself very comfortable using them across a variety of applications.
-                                My strength within this area is bringing design ideas to life in the form of websites. 
-                                Some websites I have created in the past include <a href="https://tohru.fun" rel="noopener noreferrer" target="_blank">tohru.fun</a> and <a href="https://insomniactf.com" rel="noopener noreferrer" target="_blank">insomniactf.com</a>.
-                            </p>
-                            <ul>
-                                <li>Interfaces</li>
-                                <li>User Experience</li>
-                                <li>Animations</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="langs">
-                        <div className="lang-box">
                             <h3>Javascript/Typescript/Node.js</h3>
-                            <p>3 years experience (2016)</p>
+                            <p>4 years experience (2016)</p>
                             <p>
                                 I have used Node.js and companion languages in many different applications, but the area of which
                                 I have done the most development is the creation of chat bots for social applications such as <a rel="noopener noreferrer" target="_blank" href="https://discord.gg">Discord</a>, which is a voice and chat application platform with rich
@@ -105,15 +104,18 @@ const App: React.FC = () => {
                                 <li>Back End Web Development</li>
                             </ul>
                         </div>
+                    </div>
+                    <div className="langs">
                         <div className="lang-box">
                             <h3>Python</h3>
-                            <p>2 years experience (2017)</p>
+                            <p>3 years experience (2017)</p>
                             <p>
-                                I have used python extensively in both networking and machine learning. I also use it for automation
-                                of scripts for applications in daily use and penetration testing. I professionally wrote python
+                                I have used python extensively in networking, web scraping, and general-purpose scripting. 
+                                I also use it for automation of scripts for applications in daily use and penetration testing, 
+                                an example of this being <a href="https://github.com/Shensd/icecold" target="_blank" rel="noopener noreferrer">icecold</a>, 
+                                a website wordlist generator. I utilized the python machine learning library Scikit-Learn 
                                 for the machine learning aspect of my paper <em>Use of Novelty Detection on Bash History for 
-                                Identification of Similar Source Generation in Post Attack Forensic Analysis</em>, it is currently
-                                in the process of being published by IEEE.
+                                Identification of Similar Source Generation in Post Attack Forensic Analysis.</em>
                             </p>
                             <ul>
                                 <li>Machine Learning</li>
@@ -121,30 +123,16 @@ const App: React.FC = () => {
                                 <li>Automation</li>
                             </ul>
                         </div>
-                    </div>
-                    <div className="langs">
-                        <div className="lang-box">
-                            <h3>Apache2/Linux Remote Management</h3>
-                            <p>3 years experience (2016)</p>
-                            <p>
-                                I have hosted and managed several low traffic websites for multiple years, all of which being
-                                hosted on Linux machines with Apache2 as a backend. Some examples of these can be seen 
-                                at <a href="https://insomniactf.com" rel="noopener noreferrer" target="_blank">insomniactf.com</a> and <a href="https://686f7065.pw/" rel="noopener noreferrer" target="_blank">686f7065.pw</a>.
-                            </p>
-                            <ul>
-                                <li>Server Management</li>
-                                <li>Linux Proficiency</li>
-                                <li>Web Server Security</li>
-                            </ul>
-                        </div>
                         <div className="lang-box">
                             <h3>Penetration Testing</h3>
-                            <p>1 year experience (2019)</p>
+                            <p>2 years experience (2018)</p>
                             <p>
-                                I have participated in a large amount of cybersecurity competitions, both in a team and individual settings. 
-                                Some highlights of placement are top 2% of 4000+ participants in Cyber Skyline’s NCL Regular Season 2019 CTF Competition and
-                                2nd place out of 10+ college teams in the 2019 National Cyber Summit Cyber Cup Challenge. I have found and reported 
-                                critical security exploits in ecommerce websites, of which I unfortunately cannot discuss the details here.
+                            I have participated in a large number of cybersecurity competitions, both in a team and individual 
+                            settings. These have included more game-like hacking such as capture the flag competitions, but 
+                            also more real-world such as the Collegiate Penetration Testing Competition, which is a team-based 
+                            competition where a pentest is performed on a mock business's architecture and a final penetration 
+                            testing report is written. My area of interest in penetration testing lies heavily in reverse 
+                            engineering of both desktop binaries and the deobfuscation of web scripts. 
                             </p>
                             <ul>
                                 <li>Web Application Exploitation</li>
@@ -153,12 +141,43 @@ const App: React.FC = () => {
                             </ul>
                         </div>
                     </div>
+                    <div className="langs">
+                        <div className="lang-box">
+                            <h3>HTML5/CSS3/Javascript</h3>
+                            <p>5 years experience (2015)</p>
+                            <p>
+                                I have used front end web development technologies frequently throughout my career as a 
+                                developer, and I find myself very comfortable using them across a variety of applications.
+                                My strength within this area is bringing design ideas to life in the form of websites. 
+                                Some websites I have created in the past include <a href="https://tohru.fun" rel="noopener noreferrer" target="_blank">tohru.fun</a> and <a href="https://insomniactf.com" rel="noopener noreferrer" target="_blank">insomniactf.com</a>.
+                            </p>
+                            <ul>
+                                <li>Interfaces</li>
+                                <li>User Experience</li>
+                                <li>Animations</li>
+                            </ul>
+                        </div>
+                        <div className="lang-box">
+                            <h3>Apache2/Linux Remote Management</h3>
+                            <p>4 years experience (2016)</p>
+                            <p>
+                                I have hosted and managed several low traffic websites for multiple years, all of which being
+                                hosted on Linux machines with Apache2 as a backend. An example of this can be seen 
+                                at <a href="https://insomniactf.com" rel="noopener noreferrer" target="_blank">insomniactf.com</a>.
+                            </p>
+                            <ul>
+                                <li>Server Management</li>
+                                <li>Linux Proficiency</li>
+                                <li>Web Server Security</li>
+                            </ul>
+                        </div>
+                    </div>
                 </InfoCard>
             </div>
             <div className="footer-spiral">
                 <img src={AniSpiral} alt="Spiral Animation Footer" className="img-spiral"/>
             </div>
-            <div className="footer"><p>Jack Hance 2019</p></div>
+            <div className="footer"><p>Jack Hance 2020</p></div>
         </div>
     );
 }
