77f787c4bcbd5015339a22215684dd84b63484a8
Shensd <jack@hance.xyz>
2021-03-04 15:50:05 -0600

    deleted old files

--- src/components/since.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Since = ({ children } : {children : number}) => {
-    return (
-        <p>{(new Date().getFullYear()) - children}  years experience ({children})</p>
-    )
-};
-
-export default Since;
